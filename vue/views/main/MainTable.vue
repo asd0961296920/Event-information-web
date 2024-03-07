61fc60a552dd5145b8441406fa90a0c0c6ddd90d
elim <elim@acube.com.tw>
2024-03-07 14:00:33 +0800

    feat: 更改架構

--- vue/views/main/MainTable.vue
+++ vue/views/main/MainTable.vue
@@ -2,9 +2,11 @@
 import axios from "axios";
 // import textImager from "/src/layouts/easterEggs/textImager";
 import Table from "/views/main/components/Table.vue";
+import Page from "/views/main/components/Page.vue";
 export default {
     components: {
-    Table
+    Table,
+    Page
   },
   data() {
     return {
@@ -43,14 +45,12 @@ export default {
           console.error("Error fetching data:", error);
         });
     },
-
-  handlePageClick(pageNumber) {
-
-    this.page = pageNumber;
-    this.fetchData();
-    },
   PageClick(id) {
 window.location.href = "/page/"+id;
+    },
+    PageData(number){
+      this.page = number;
+      this.fetchData();
     }
 
 
@@ -58,18 +58,6 @@ window.location.href = "/page/"+id;
 
   // 计算属性，用于根据搜索条件过滤数据
   computed: {
-    displayedPageNumbers() {
-      const maxDisplayedPages = 10;
-      const startPage = Math.max(1, this.currentPage - Math.floor(maxDisplayedPages / 2));
-      const endPage = Math.min(this.pageData.totalPages, startPage + maxDisplayedPages - 1);
-      const displayedPages = [];
-
-      for (let i = startPage; i <= endPage; i++) {
-        displayedPages.push(i);
-      }
-
-      return displayedPages;
-    },
   },
 };
 </script>
@@ -78,40 +66,7 @@ window.location.href = "/page/"+id;
   <div>
     <Table :apiData="apiData" />
 
-
-
-<nav aria-label="Page navigation example">
-  <ul class="pagination">
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous" @click="handlePageClick(1)">
-        <span aria-hidden="true">&laquo;</span>
-      </a>
-    </li>
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous" @click="handlePageClick(page > 1 ? page - 1 : 1)">
-        <span aria-hidden="true"> &lt; </span>
-      </a>
-    </li>
-<div v-for="counter in displayedPageNumbers" :key="counter"> 
-    <li class="page-item active" aria-current="page" v-if="counter == page">
-      <a class="page-link" href="#" @click="handlePageClick(counter)">{{ counter }}</a>
-      </li>
-          <li class="page-item" v-else>
-      <a class="page-link" href="#" @click="handlePageClick(counter)">{{ counter }}</a>
-      </li>
-</div>
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next" @click="handlePageClick(page < pageData.totalPages ? page + 1 : pageData.totalPages)">
-        <span aria-hidden="true"> &gt; </span>
-      </a>
-    </li>
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next" @click="handlePageClick(pageData.totalPages)">
-        <span aria-hidden="true">&raquo;</span>
-      </a>
-    </li>
-  </ul>
-</nav>
+ <Page :pageData="pageData" @handlePageClick="PageData" />
 
 
   </div>
