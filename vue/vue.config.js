2c2e57944b4cfafa8b91b6bc434dcf2211b99e09
asd0961296920 <59499308+asd0961296920@users.noreply.github.com>
2024-04-30 15:40:02 +0800

    feat:

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,3 +1,43 @@
+let routes = [
+    '/', '/city', '/page','/city/:id', '/page/:id'
+  ]
+
+  const SitemapWebpackPlugin = require('sitemap-webpack-plugin').default;
+
 module.exports = {
-    transpileDependencies: ["@vueform"]
+    transpileDependencies: ["@vueform"],
+    
+configureWebpack: {
+    plugins: [
+      new SitemapWebpackPlugin({
+        base: 'https://twgather.techscomet.com',
+        paths: routes,
+        options: {
+          //生成的站点地图文件名
+          filename: 'sitemap.xml',
+          // lastMod属性是否开启
+          lastMod: true,
+          // 更新频率
+          changefreq: 'monthly'
+        }
+      })
+    ]
+  }
 }
+
+
+
+// config.plugin("sitemap").use(sitemapWebpackPlugin, [
+//     {
+//       base: 'https://twgather.techscomet.com',
+//       paths: routes,
+//       options: {
+//         //生成的站点地图文件名
+//         filename: 'sitemap.xml',
+//         // lastMod属性是否开启
+//         lastMod: true,
+//         // 更新频率
+//         changefreq: 'monthly'
+//       },
+//     }
+//   ])
